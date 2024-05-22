7b31cc3343849d795ef7eb957c03760d2a3c89cf
Goodnessmbakara <amicablembakara50@gmail.com>
2024-05-22 13:53:44 +0100

    made fields optional

--- exprosys_app/models.py
+++ exprosys_app/models.py
@@ -108,10 +108,10 @@ class Customer(models.Model):
     state_province = models.CharField(max_length=100)
     postal_code = models.CharField(max_length=20)
     country = models.CharField(max_length=100)
-    account_balance = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
-    credit_limit = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
-    payment_terms = models.CharField(max_length=100)
-    credit_status = models.CharField(max_length=50)
+    account_balance = models.DecimalField(max_digits=10, decimal_places=2, default=0.00, null =True, blank=True)
+    credit_limit = models.DecimalField(max_digits=10, decimal_places=2, default=0.00, null =True, blank=True)
+    payment_terms = models.CharField(max_length=100, null =True, blank=True)
+    credit_status = models.CharField(max_length=50, null =True, blank=True)
 
     def __str__(self):
         return self.customer_name
